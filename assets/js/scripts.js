b9897ad0db5a45f3af2a76bdbd1e80696fb0237f
Peace Alawode <peace.alawode@hotmail.co.uk>
2023-11-26 22:51:27 +0000

    finalised design for highlights and news section

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -23,25 +23,55 @@ const highlightVideoHeader = document.querySelector('.highlightVideoHeader');
 const highlightVideoCompetition = document.querySelector('.highlightVideoCompetition');
 const highlightVideoDate = document.querySelector('.highlightVideoDate');
 
+const videoCarouselContainer = document.querySelector('#videoCarouselContainer');
+
 const highlightVideoURL = 'https://www.scorebat.com/video-api/v3/feed/?token=MTMyMDY5XzE3MDA5NDgwODZfMGMzMmU0MzAyMDliNWE2ODZiYmE3ZDc3ZTU1OTU4MjIyYzg3YmVhNw==';
 
 fetch(highlightVideoURL)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
-    // fetch latest video in video container in DOM
-    highlightVideo.innerHTML = data.response[0].videos[0].embed;
+        // fetch latest video in video container in DOM
+        highlightVideo.innerHTML = data.response[0].videos[0].embed;
 
-    // fetch header
-    highlightVideoHeader.innerText = `${data.response[0].title} | ${data.response[0].videos[0].title}`;
+        // fetch header
+        highlightVideoHeader.innerText = `${data.response[0].title} | ${data.response[0].videos[0].title}`;
 
-    // fetch competition
-    highlightVideoCompetition.innerText = data.response[0].competition;
+        // fetch competition
+        highlightVideoCompetition.innerText = data.response[0].competition;
 
-    // fetch competition
-    dateTime = new Date(data.response[0].date);
-    const formattedDate = dateTime.toLocaleDateString('en-UK', { year: 'numeric', month: '2-digit', day: '2-digit' });
-    const formattedTime = dateTime.toLocaleTimeString('en-UK', { hour: '2-digit', minute: '2-digit' });
-    highlightVideoDate.innerText = `${formattedDate} | ${formattedTime}`
+        // fetch competition
+        dateTime = new Date(data.response[0].date);
+        const formattedDate = dateTime.toLocaleDateString('en-UK', { year: 'numeric', month: '2-digit', day: '2-digit' });
+        const formattedTime = dateTime.toLocaleTimeString('en-UK', { hour: '2-digit', minute: '2-digit' });
+        highlightVideoDate.innerText = `${formattedDate} | ${formattedTime}`
+
+        // fetch list of up to 7 other videos to be put into a carousel and when a video is clicked, it is played in main highlightVideo
+        const responseArray = data.response.slice(1, 11);
+        responseArray.forEach(response => {
+            // create video container
+            const videoContainer = document.createElement('img');
+            videoContainer.classList.add('videoContainer');
+            videoContainer.src = response.thumbnail;
+            videoContainer.style.cursor = 'pointer';
+            videoCarouselContainer.appendChild(videoContainer);
+            
+            videoContainer.addEventListener('click', () => {
+                // fetch clicked video in video container in DOM
+                highlightVideo.innerHTML = response.videos[0].embed;
+
+                // fetch header
+                highlightVideoHeader.innerText = `${response.title} | ${response.videos[0].title}`;
+
+                // fetch competition
+                highlightVideoCompetition.innerText = response.competition;
+
+                // fetch competition
+                dateTime = new Date(response.date);
+                const formattedDate = dateTime.toLocaleDateString('en-UK', { year: 'numeric', month: '2-digit', day: '2-digit' });
+                const formattedTime = dateTime.toLocaleTimeString('en-UK', { hour: '2-digit', minute: '2-digit' });
+                highlightVideoDate.innerText = `${formattedDate} | ${formattedTime}`
+            })
+        })
 
     })
     .catch(err => {
@@ -100,6 +130,30 @@ fetch(newsURL)
     .catch(err => {
         console.log(`error ${err}`)
     });
+
+
+
+
+const fixturesurl = 'https://football98.p.rapidapi.com/premierleague/fixtures';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3fd9b7f2c5msh54e654c2fb9f2b7p11e360jsn06884891fa6f',
+		'X-RapidAPI-Host': 'football98.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+
+
+
+
 
 /*
 document.querySelector('button').addEventListener('click', getFetch)
